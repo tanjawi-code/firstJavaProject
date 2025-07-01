8e76d1169db48c94e5edb2ccf10f3ef27c827f2a
TANJAWI <ahmedahmed2007tanjawi@gmail.com>
2025-07-01 19:22:05 +0100

    I learned how to use arrays with objects

--- src/Main.java
+++ src/Main.java
@@ -4,20 +4,42 @@ public class Main {
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
 
-        Student[] Students = new Student[3];
+        System.out.print("Enter the number of the students : ");
+        int number = input.nextInt();
+        input.nextLine();
 
-        Student s1 = new Student("ahmed",17);
-        Student s2 = new Student("mohammed",16);
-        Student s3 = new Student("yossef",18);
+        Student[] Students = new Student[number];
 
-        Students[0] = s1;
-        Students[1] = s2;
-        Students[2] = s3;
-
+        for(int x = 0; x<Students.length;x++){
+            Students[x] = new Student();
+            System.out.print("Enter your name : ");
+            String name = input.nextLine();
+            System.out.print("Enter your age : ");
+            int age = input.nextInt();
+            input.nextLine();
+            Students[x].setDetails(name,age);
+            System.out.println();
+        }
 
         for(int x = 0 ; x<Students.length ; x++){
-            Students[x].info();
+            System.out.println("Enter grades for the student "+(x+1)+" : ");
+            for(int y = 0; y<3; y++){
+                System.out.print("Enter the grade number "+(y+1)+" : ");
+                float grade = input.nextInt();
+                Students[x].setGrades(grade,y);
+            }
         }
+
+        for(int y = 0 ; y<Students.length ; y++) {
+            Students[y].info();
+            System.out.println();
+        }
+
+        for(int x = 0; x<Students.length ; x++){
+            double point = Students[x].calculateGrades();
+            System.out.println("The student : "+point);
+        }
+
 
 
         input.close();
--- src/Student.java
+++ src/Student.java
@@ -2,10 +2,9 @@ public class Student {
 
     private String name;
     private int age;
+    private float[] grades = new float[3];
 
-    public Student(String name , int age){
-        this.name = name;
-        this.age = age;
+    public Student(){
     }
 
     public void setDetails(String name , int age){
@@ -13,15 +12,19 @@ public class Student {
         this.age = age;
     }
 
-    public String getName(){
-        return name;
-    }
-    public int getAge(){
-        return age;
-    }
-
     public void info(){
         System.out.println("The name is : "+name);
         System.out.println("The age : "+age);
+    }
+
+    public float getGrades(int index) {
+        return grades[index];
+    }
+    public void setGrades(float grades , int index) {
+        this.grades[index] = grades;
+    }
+
+    public double calculateGrades(){
+       return this.grades[0] + this.grades[1] + this.grades[2];
     }
 }
